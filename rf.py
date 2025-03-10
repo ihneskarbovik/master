17b319b2a722567007ee1e4677366f50c8e9c8b8
Ihne Skarbøvik <ihnes@uio.no>
2025-03-10 12:34:20 +0100

    🦯

--- rf.py
+++ rf.py
@@ -1,10 +1,15 @@
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler
 
-def rf(X_train, X_test, mode_train, mode_test, campaigns, test_campaigns, features, n_estimators=100):
-    rf = RandomForestClassifier(n_estimators=n_estimators)
+def rf(X_train, X_test, mode_train, mode_test, campaigns, test_campaigns, features, n_estimators=100, max_depth=None, min_samples_leaf=1):
+    permutation = np.random.permutation(X_train.index)
+    X_train = X_train.reindex(permutation).reset_index(drop=True)
+    mode_train = mode_train.reindex(permutation).reset_index(drop=True)
+
+    rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, min_samples_leaf=min_samples_leaf)
     smote = SMOTE()
     rus = RandomUnderSampler()
     X_train_balanced, mode_train_balanced = smote.fit_resample(X_train[features], mode_train['Mode'])
@@ -17,35 +22,47 @@ def rf(X_train, X_test, mode_train, mode_test, campaigns, test_campaigns, featur
         accuracy = accuracy_score(mode_test['Mode'], y_pred)
         precision = precision_score(mode_test['Mode'], y_pred)
         recall = recall_score(mode_test['Mode'], y_pred)
+
+        y_true = mode_test
     else:
         X_test_test = X_test[X_test['campaign'] == test_campaigns[0]]
         X_test_mode = mode_test[mode_test['campaign'] == test_campaigns[0]]
         
         y_pred = rf.predict(X_test_test[features])
-        accuracy = accuracy_score(X_test_mode['Mode'], y_pred)
-        precision = precision_score(X_test_mode['Mode'], y_pred)
-        recall = recall_score(X_test_mode['Mode'], y_pred)
+
+        y_pred_total = rf.predict(X_test[features])
+        accuracy = accuracy_score(mode_test['Mode'], y_pred_total)
+        precision = precision_score(mode_test['Mode'], y_pred_total)
+        recall = recall_score(mode_test['Mode'], y_pred_total)
+
+        y_true = X_test_mode
 
     if len(campaigns) == 1:
         train_pred = rf.predict(X_train[features])
         train_accuracy = accuracy_score(mode_train['Mode'], train_pred)
         train_precision = precision_score(mode_train['Mode'], train_pred)
         train_recall = recall_score(mode_train['Mode'], train_pred)
+
+        train_true = mode_train
     else:
         X_train_test = X_train[X_train['campaign'] == campaigns[0]]
         X_train_mode = mode_train[mode_train['campaign'] == campaigns[0]]
 
         train_pred = rf.predict(X_train_test[features])
-        train_accuracy = accuracy_score(X_train_mode['Mode'], train_pred)
-        train_precision = precision_score(X_train_mode['Mode'], train_pred)
-        train_recall = recall_score(X_train_mode['Mode'], train_pred)
+
+        train_pred_total = rf.predict(X_train[features])
+        train_accuracy = accuracy_score(mode_train['Mode'], train_pred_total)
+        train_precision = precision_score(mode_train['Mode'], train_pred_total)
+        train_recall = recall_score(mode_train['Mode'], train_pred_total)
+
+        train_true = X_train_mode
 
     results = {'Accuracy': round(accuracy, 3),
                'Precision': round(precision, 3),
                'Recall': round(recall, 3),
-               'y_true' : mode_test,
+               'y_true' : y_true,
                'y_pred' : y_pred,
-               'train_true': mode_train,
+               'train_true': train_true,
                'train_pred': train_pred,
                'Accuracy_train': round(train_accuracy, 3),
                'Precision_train': round(train_precision, 3),
